4095198e6f902d5db1accfc6da19534feea5fdd0
Jwatts82 <wattscodes@gmail.com>
2023-01-17 15:47:09 -0500

    nest json

--- bucketlist-backend/app/serializers/item_serializer.rb
+++ bucketlist-backend/app/serializers/item_serializer.rb
@@ -1,3 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :description
+  belongs_to :list
 end
--- bucketlist-backend/app/serializers/list_serializer.rb
+++ bucketlist-backend/app/serializers/list_serializer.rb
@@ -1,3 +1,4 @@
 class ListSerializer < ActiveModel::Serializer
   attributes :id, :description, :completed
+  has_many :items
 end
